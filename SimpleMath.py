8711b3b8735ee17304024d7edb524eb80d1368b1
Sam Liebert <83291234+seesi8@users.noreply.github.com>
2023-06-20 22:00:56 -0500

    Update SimpleMath.py

--- SimpleMath.py
+++ SimpleMath.py
@@ -10,98 +10,90 @@ class SimpleMath:
     def add(self, a, b):
         """
         Adds two numbers and returns the result.
-    
-        Args:
-            self: The object instance.
-            a (number): The first number to be added.
-            b (number): The second number to be added.
-    
-        Returns:
-            number: The sum of the two numbers.
-    
-        Raises:
-            None
-    
-        Examples:
-            >>> obj = ClassName()
-            >>> obj.add(3, 4)
-            7
+
+        :param self: The object instance.
+        :param a: The first number to be added.
+        :param b: The second number to be added.
+        :type a: number
+        :type b: number
+        :return: The sum of the two numbers.
+        :rtype: number
+        :raises: None
+
+        Example:
+        >>> obj = ClassName()
+        >>> obj.add(3, 4)
+        7
         """
         a = int(a)
         b = int(b)
-        return Response(suceeded=True, data=a+b)
+        return Response(succeeded=True, data=a+b)
 
     @reg(name="Multiply")
     def multiply(self, a, b):
         """
         Multiplies two numbers and returns the result.
-    
-        Args:
-            self: The object instance.
-            a (number): The first number to be multiplied.
-            b (number): The second number to be multiplied.
-    
-        Returns:
-            number: The product of the two numbers.
-    
-        Raises:
-            None
-    
-        Examples:
-            >>> obj = ClassName()
-            >>> obj.multiply(3, 4)
-            12
+
+        :param self: The object instance.
+        :param a: The first number to be multiplied.
+        :param b: The second number to be multiplied.
+        :type a: number
+        :type b: number
+        :return: The product of the two numbers.
+        :rtype: number
+        :raises: None
+
+        Example:
+        >>> obj = ClassName()
+        >>> obj.multiply(3, 4)
+        12
         """
         a = int(a)
         b = int(b)
-        return Response(suceeded=True, data=a*b)
+        return Response(succeeded=True, data=a*b)
 
     @reg(name="Subtract")
     def subtract(self, a, b):
         """
         Subtracts one number from another and returns the result.
-    
-        Args:
-            self: The object instance.
-            a (number): The number to be subtracted from.
-            b (number): The number to subtract.
-    
-        Returns:
-            number: The difference between the two numbers.
-    
-        Raises:
-            None
-    
-        Examples:
-            >>> obj = ClassName()
-            >>> obj.subtract(7, 3)
-            4
+
+        :param self: The object instance.
+        :param a: The number to be subtracted from.
+        :param b: The number to subtract.
+        :type a: number
+        :type b: number
+        :return: The difference between the two numbers.
+        :rtype: number
+        :raises: None
+
+        Example:
+        >>> obj = ClassName()
+        >>> obj.subtract(7, 3)
+        4
         """
         a = int(a)
         b = int(b)
-        return Response(suceeded=True, data=a-b)
+        return Response(succeeded=True, data=a-b)
 
     @reg(name="Divide")
     def divide(self, a, b):
         """
         Divides one number by another and returns the result.
-    
-        Args:
-            self: The object instance.
-            a (number): The dividend.
-            b (number): The divisor.
-    
-        Returns:
-            number: The quotient of the division.
-    
-        Raises:
-            None
-    
-        Examples:
-            >>> obj = ClassName()
-            >>> obj.divide(10, 2)
-            5.0
+
+        :param self: The object instance.
+        :param a: The dividend.
+        :param b: The divisor.
+        :type a: number
+        :type b: number
+        :return: The quotient of the division.
+        :rtype: number
+        :raises: None
+
+        Example:
+        >>> obj = ClassName()
+        >>> obj.divide(10, 2)
+        5.0
         """
         a = int(a)
         b = int(b)
-        return Response(suceeded=True, data=a/b)
+        return Response(succeeded=True, data=a/b)
